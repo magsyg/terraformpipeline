93107e20d529731a9d455bdda607fab76ee3e537
magsyg <magsyg@github.com>
2025-01-09 21:31:17 +0100

    initial files

---
+++ terraform.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "eu-north-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "magnussoprahello"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "blog" {
+  bucket = aws_s3_bucket.bucket.id
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+##### will upload all the files present under HTML folder to the S3 bucket #####
+resource "aws_s3_object" "upload_object" {
+  for_each      = fileset("src/", "*")
+  bucket        = aws_s3_bucket.bucket.id
+  key           = each.value
+  source        = "src/${each.value}"
+  etag          = filemd5("src/${each.value}")
+  content_type  = "text/html"
+}
