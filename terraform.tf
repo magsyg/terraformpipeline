d359c2f08617c6dde505f38ea74f23d67e968586
magsyg <magsyg@github.com>
2025-01-13 10:42:30 +0100

    simplebucket

--- terraform.tf
+++ terraform.tf
@@ -3,23 +3,8 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket        = "workingbucket"
+  bucket        = "magnussopraworkingbucket"
   force_destroy = true
-}
-
-resource "aws_s3_bucket_website_configuration" "test" {
-  bucket = aws_s3_bucket.bucket.id
-  index_document {
-    suffix = "index.html"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "public_access_block" {
-  bucket                  = aws_s3_bucket.bucket.id
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
 
 resource "aws_s3_object" "upload_object" {
