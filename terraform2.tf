815c6970feffc0c3bdd74ba192b75a30d9fec36f
magsyg <magsyg@github.com>
2025-01-13 10:59:11 +0100

    new bucket

--- terraform2.tf
+++ terraform2.tf
@@ -2,16 +2,7 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_s3_bucket" "bucket" {
+resource "aws_s3_bucket" "bucket2" {
   bucket        = "magnussopraworkingbucket"
   force_destroy = true
-}
-
-resource "aws_s3_object" "upload_object" {
-  for_each     = fileset("src/", "*")
-  bucket       = aws_s3_bucket.bucket.id
-  key          = each.value
-  source       = "src/${each.value}"
-  etag         = filemd5("src/${each.value}")
-  content_type = "text/html"
 }
