a54cecea7a5908972cd3d09d6ab07272b70be5c5
magsyg <magsyg@github.com>
2025-01-13 10:59:37 +0100

    new bucket

--- terraform2.tf
+++ terraform2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_s3_bucket" "bucket2" {
+resource "aws_s3_bucket2" "bucket" {
   bucket        = "magnussopraworkingbucket"
   force_destroy = true
 }
